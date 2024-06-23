5f730ba673636db09823b281fbc84d5716f0a42a
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-24 08:29:33 +0900

    Create gif.js

---
+++ modules/commands/gif.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+const cooldowns = new Map();
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('search-gif')
+    .setDescription('指定したワードに関するGIFを送信します')
+    .addStringOption(option =>
+      option.setName('query')
+        .setDescription('検索したいワード')
+        .setRequired(true)),
+  async execute(interaction) {
+
+    // クールダウン中の処理
+    if (cooldowns.has(interaction.guildId)) {
+      const cooldown = cooldowns.get(interaction.guildId);
+      const now = Date.now();
+      if (cooldown > now) {
+        const remainingTime = Math.ceil((cooldown - now) / 1000); 
+        await interaction.reply({ content: `クールダウン中です。\nあと${remainingTime} 秒後にもう一度やってね！`, ephemeral: true });
+        return;
+      }
+    }
+
+    // apiキーと検索ワードの取得
+    const apiKey = process.env.tenorAPI;
+    const query = interaction.options.getString('query');
+
+    await interaction.deferReply();
+
+    try {
+      // tenorAPIに投げる
+      const response = await axios.get(`https://tenor.googleapis.com/v2/search?q=${query}&key=${apiKey}&random=true`);
+      const gifUrl = response.data.results[0].media_formats.gif.url;
+
+      // embedを送信
+      const embed = new EmbedBuilder()
+        .setColor('#f8b4cb')
+        .setTimestamp()
+        .setFooter({ text:'Emubot | search-GIF'})
+        .setTitle(`${query}のGIFです！`)
+        .setImage(gifUrl);
+
+      await interaction.editReply({ embeds: [embed] });
+    } catch (error) {
+      console.error('gif nai..');
+      await interaction.editReply('GIFが見つかりませんでした');
+    }
+
+    // 15秒のクールダウンの設定
+    const cooldownTime = 15000; 
+    cooldowns.set(interaction.guildId, Date.now() + cooldownTime);
+    setTimeout(() => cooldowns.delete(interaction.guildId), cooldownTime);
+  },
+};
