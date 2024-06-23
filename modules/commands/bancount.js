af935a9bb296899963decb2b572ce300f952cabe
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-24 06:59:08 +0900

    Create bancount.js

---
+++ modules/commands/bancount.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bancount')
+        .setDescription('BANされているユーザーのカウント'),
+
+    async execute(interaction) {
+        try {
+            if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.ViewAuditLog)) {
+                return interaction.reply({ content: '監査ログの閲覧権限がありません', ephemeral: true });
+            }
+
+            const bannedUsers = await interaction.guild.bans.fetch();
+            const bannedUsersCount = bannedUsers.size;
+
+            const embed = new EmbedBuilder()
+                .setColor('#f8b4cb')
+                .setTitle('Banned Users')
+                .setTimestamp()
+                .setFooter({ text:'Emubot | bancount'})
+                .setDescription(`このサーバーのBANユーザー数: ${bannedUsersCount}`);
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+        }
+    },
+};
