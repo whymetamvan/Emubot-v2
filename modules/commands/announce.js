03a90ed772153e0658e2b122722083d239e610f1
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-24 08:27:01 +0900

    Update announce.js

--- modules/commands/announce.js
+++ modules/commands/announce.js
@@ -6,8 +6,10 @@ module.exports = {
         .setDescription('お知らせをするえむbot開発室を作ります'),
     async execute(interaction) {
         const guild = interaction.guild;
+        // 作成するチャンネル名
         const channelName = 'えむbot開発室';
 
+// ユーザーとbotの権限の確認
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
             return interaction.reply({ content: 'あなたに実行権限が有りません。', ephemeral: true });
         }
@@ -16,16 +18,18 @@ module.exports = {
             return interaction.reply({ content: 'チャンネルの作成権限がありません。', ephemeral: true });
         }
 
+// えむbot開発室が既にある場合の処理
         const existingChannel = guild.channels.cache.find(channel => channel.name === channelName && channel.type === 0);
 
         if (existingChannel) {
             await interaction.reply({ content: 'えむbot開発室は既にあります', ephemeral: true });
-        } else {
+        } else {　// なかった場合は作成
             try {
                 const createdChannel = await guild.channels.create({
                     name: channelName,
-                    type: 0
+                    type: 0 // テキストチャンネルです
                 });
+                // embedを送信
                 const embed = new EmbedBuilder()
                     .setColor('#f8b4cb')
                     .setTitle('えむbot開発室')
