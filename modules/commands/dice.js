12634728baa5165f55723c2d3c06a7986994002b
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-24 08:29:16 +0900

    Create dice.js

---
+++ modules/commands/dice.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder,EmbedBuilder } = require('discord.js');
+const path = require('path');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('dice')
+        .setDescription('サイコロをふります'),
+    
+    async execute(interaction) {
+        try {
+            await interaction.deferReply();
+            const thumbnailPath = path.join(__dirname, '..', '..', 'lib', 'images', 'dice.gif');
+            const diceRoll = Math.floor(Math.random() * 6) + 1;
+
+            // embedを送信
+            const embed = new EmbedBuilder()
+                .setColor('#f8b4cb')
+                .setTitle('サイコロ 🎲')
+                .setTimestamp()
+                  .setFooter({ text:'Emubot | dice'})
+.setThumbnail(`attachment://${path.basename(thumbnailPath)}`)
+                .setDescription(`サイコロの目は \`${diceRoll}\` です！`);
+
+            await interaction.editReply({ embeds: [embed],files: [{attachment:thumbnailPath,name:path.basename(thumbnailPath)}] });
+        } catch (error) {
+            console.error('diceエラー',error);
+
+            try {
+                await interaction.editReply({ content: 'エラーが発生しました', ephemeral: true });
+            } catch (replyError) {
+                console.error('Replyエラー', replyError);
+            }
+        }
+    },
+};
