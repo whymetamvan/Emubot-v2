8e9a51d3f70d5ca2f509198d00517f213e5bcd42
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-24 09:33:32 +0900

    Create role count.js

---
+++ modules/commands/role count.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
+
+module.exports = {
+data: new SlashCommandBuilder()
+.setName('roles')
+.setDescription('サーバー内のロール一覧を表示します。'),
+async execute(interaction) {
+  try {
+    await interaction.deferReply({ ephemeral:true });
+
+    // botの権限確認
+    if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageRoles)) {
+        return interaction.editReply({ content: 'ロールの閲覧権限がありません。'});
+    }
+
+    // ロールの取得、embedの送信
+    const roles = interaction.guild.roles.cache;
+    const sortedRoles = roles.sort((a, b) => b.position - a.position);
+    const roleList = sortedRoles.map(r => `${r}`).join("\n");
+
+    const embed = new EmbedBuilder()
+    .setTitle('サーバーのロール一覧')
+    .setTimestamp()
+    .setFooter({ text:'Emubot | roles'})
+    .setDescription(`>>> ${roleList}`)
+    .setColor('#f8b4cb');
+
+    await interaction.editReply({ embeds: [embed] });
+  } catch (error) {
+    console.error('エラーが発生しました:', error);
+    await interaction.editReply({ content: 'コマンドの実行中にエラーが発生しました。'});
+  }
+},
+};
