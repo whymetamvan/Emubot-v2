f75e1ef2bd7686b1261643073aa1d9ef04c11825
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-24 10:30:40 +0900

    Create whois.js

---
+++ modules/commands/whois.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+const path = require('path');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('whois')
+        .setDescription('whois情報の表示')
+        .addStringOption(option =>
+            option.setName('domain')
+                .setDescription('調べたいドメイン')
+                .setRequired(true)),
+    async execute(interaction) {
+
+        await interaction.deferReply();
+      // ドメインとAPIを取得、ip2whoisAPIに投げる
+        const domain = interaction.options.getString('domain');
+        const thumbnailPath = path.join(__dirname, '..', '..', 'lib', 'images', 'whois.png');
+        const ip2_API = process.env.ip_API;
+        try {
+
+            const response = await axios.get(`https://api.ip2whois.com/v2?key=${ip2_API}&domain=${domain}`);
+            const data = response.data;
+
+          // embedを送信
+            const embed = new EmbedBuilder()
+                .setTitle(`WHOIS Lookup for ${domain}`)
+                .addFields(
+                    { name: '作成日', value: data.create_date || 'None' },
+                    { name: '更新日', value: data.update_date || 'None' },
+                    { name: '有効期限', value: data.expire_date || 'None' },
+                    { name: '登録者名', value: data.registrant.name || 'None' },
+                    { name: '登録者地域', value: data.registrant.city || 'None' },
+                    { name: 'email', value: data.registrant.email || 'None' },
+                )
+                .setColor('#f8b4cb')
+                .setTimestamp()
+                .setFooter({ text: "Emutest | whois"})
+.setThumbnail(`attachment://${path.basename(thumbnailPath)}`)
+                .setTimestamp();
+
+          await interaction.editReply(
+            { embeds: [embed],files: [{attachment:thumbnailPath,name:path.basename(thumbnailPath)}] });        } catch (error) {
+            console.log('whois error');
+            await interaction.editReply('whois検索に失敗しました');
+        }
+    },
+};
