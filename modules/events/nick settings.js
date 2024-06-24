f953aaedad557a313f1e706c5933079a902440d1
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-24 12:53:19 +0900

    Update nick settings.js

--- modules/events/nick settings.js
+++ modules/events/nick settings.js
@@ -1,14 +1,22 @@
+const config = require('../data/config.json');
+
 module.exports = {
   name: 'ready',
   execute(client) {
     client.guilds.cache.forEach(guild => {
       guild.members.fetch(client.user.id)
         .then(member => {
-          member.setNickname('Emu V2')
+          const nickname = config.nickname; 
+          member.setNickname(nickname)
             .catch(error => console.error(`${guild.name}のニックネームの変更失敗`));
-        })
+        });
     });
 
     client.user.setStatus('online');
+   setInterval(() => {
+　　　　 const activities = ["えむbotだよ！", "/youtube-playを実装"];
+        const randomActivity = activities[Math.floor(Math.random() * activities.length)];
+        client.user.setActivity(randomActivity);
+      }, 10000);
   }
 };
