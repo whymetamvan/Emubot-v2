f605ec802fa28535f4e71233da2554c6b6ed1826
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-02 06:52:07 +0900

    Update serverlist.js

--- modules/commands/serverlist.js
+++ modules/commands/serverlist.js
@@ -8,21 +8,30 @@ module.exports = {
         try {
             await interaction.deferReply({ ephemeral: true});
 
-            const guilds = interaction.client.guilds.cache;
-            const guildNames = guilds.map(guild => guild.name).join('\n');
-            const guildCount = guilds.size;
+            // 参加サーバーを取得
+            const guilds = interaction.client.guilds.cache.map(guild => guild.name);
+            const guildCount = guilds.length;
 
+            // 2列に分けるための処理
+            const half = Math.ceil(guilds.length / 2);
+            const guildsFirstHalf = guilds.slice(0, half);
+            const guildsSecondHalf = guilds.slice(half);
+
+            // embedを作成
             const embed = new EmbedBuilder()
                 .setColor('#f8b4cb')
                 .setTimestamp()
                 .setFooter({ text: "Emutest | serverlist" })
                 .setTitle(`サーバー一覧（現在: ${guildCount}）`)
-                .setDescription(guildNames);
+                .addFields(
+                    { name: 'list1', value: guildsFirstHalf.join('\n'), inline: true },
+                    { name: 'list2', value: guildsSecondHalf.join('\n'), inline: true }
+                );
 
             await interaction.editReply({ embeds: [embed] });
         } catch (error) {
             console.error(error);
-            await interaction.editReply({ content: 'コマンドの実行中にエラーが発生しました。もう一度お試しください。', ephemeral: true });
+            await interaction.editReply({ content: 'エラーが発生しました。', ephemeral: true });
         }
     },
 };
