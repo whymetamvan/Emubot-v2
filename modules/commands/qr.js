e838be6718a99dae876a8d4c51bb08ef51a88d24
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-13 17:22:31 +0900

    Update qr.js

--- modules/commands/qr.js
+++ modules/commands/qr.js
@@ -1,31 +1,80 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('qr')
-        .setDescription('QRコードを生成するよ')
+        .setDescription('QRコードの生成とQRコードの読み取りを行います')
+        .addStringOption(option =>
+            option.setName('action')
+                .setDescription('実行したい操作を選択してください')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'URLをQRコードに変換', value: 'generate' },
+                    { name: 'QRコードをURLに変換', value: 'read' }
+                ))
         .addStringOption(option =>
             option.setName('url')
-                .setDescription('QRにしたいURLを入力してね')
-                .setRequired(true)),
+                .setDescription('QRに変換したいURLを入力してください')
+                .setRequired(false))
+        .addAttachmentOption(option =>
+            option.setName('file')
+                .setDescription('読み取りたいQRコードの画像をアップロードしてください')
+                .setRequired(false)),
 
     async execute(interaction) {
+        await interaction.deferReply();
         try {
-          // URLを取得
+            const action = interaction.options.getString('action');
             const url = interaction.options.getString('url');
+            const file = interaction.options.getAttachment('file');
 
-          // embedを送信
-            const embed = new EmbedBuilder()
-                .setColor('#f8b4cb')
-                .setTitle('QRコードにしました！')
-                .setTimestamp()
-                .setFooter({ text:'Emubot | qr'})
-          　    .setImage(`https://qrickit.com/api/qr.php?d=${encodeURIComponent(url)}`);
+            if (action === 'generate') {
+                if (!url) {
+                    await interaction.editReply({ content: 'QRコードに変換したいURLを入力してください。', ephemeral: true });
+                    return;
+                }
 
-            await interaction.reply({ embeds: [embed] , ephemeral: true});
+                // URLをQRコードに変換
+                const embed = new EmbedBuilder()
+                    .setColor('#f8b4cb')
+                    .setTitle('QRコードにしました！')
+                    .addFields({ name:'URL', value:url })
+                    .setTimestamp()
+                    .setFooter({ text: 'Emubot | qr', iconURL: 'https://mpreview.aflo.com/pCiOjL55MseL/afloimagemart_217582635.jpg' })
+                    .setImage(`https://qrickit.com/api/qr.php?d=${encodeURIComponent(url)}`);
+
+                await interaction.editReply({ embeds: [embed], ephemeral: true });
+            } else if (action === 'read') {
+                if (!file) {
+                    await interaction.editReply({ content: 'QRコードの画像をアップロードしてください。', ephemeral: true });
+                    return;
+                }
+
+                // QRコードをURLに変換
+                const apiUrl = `https://api.qrserver.com/v1/read-qr-code/?fileurl=${encodeURIComponent(file.url)}`;
+
+                const response = await axios.get(apiUrl);
+                const data = response.data;
+
+                if (data && data[0] && data[0].symbol[0] && data[0].symbol[0].data) {
+                    const decodedUrl = data[0].symbol[0].data;
+
+                    const embed = new EmbedBuilder()
+                        .setColor('#f8b4cb')
+                        .setTitle('QRコードを読み取りました！')
+                        .addFields({ name:'URL', value:decodedUrl })
+                        .setTimestamp()
+                        .setFooter({ text: 'Emubot | qr', iconURL: 'https://mpreview.aflo.com/pCiOjL55MseL/afloimagemart_217582635.jpg' });
+
+                    await interaction.editReply({ embeds: [embed], ephemeral: true });
+                } else {
+                    await interaction.editReply({ content: 'QRコードの読み取りに失敗しました。画像が正しいか確認してください。', ephemeral: true });
+                }
+            }
         } catch (error) {
             console.error(error);
-            await interaction.reply('エラーが発生しました');
+            await interaction.editReply('エラーが発生しました。');
         }
     },
 };
