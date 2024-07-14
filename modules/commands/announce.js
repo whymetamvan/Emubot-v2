61d90d52b29e04172fdee6d14a2d5c2899f656fc
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-14 11:09:17 +0900

    Update announce.js

--- modules/commands/announce.js
+++ modules/commands/announce.js
@@ -7,9 +7,10 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply();
         const guild = interaction.guild;
+        // 作成するチャンネル名
         const channelName = 'えむbot開発室';
 
-        // ユーザーとbotの権限の確認
+// ユーザーとbotの権限の確認
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
             return interaction.editReply({ content: 'あなたに実行権限が有りません。', ephemeral: true });
         }
@@ -18,44 +19,31 @@ module.exports = {
             return interaction.editReply({ content: 'チャンネルの作成権限がありません。', ephemeral: true });
         }
 
-        // えむbot開発室が既にある場合の処理
+// えむbot開発室が既にある場合の処理
         const existingChannel = guild.channels.cache.find(channel => channel.name === channelName && channel.type === ChannelType.GuildText);
 
         if (existingChannel) {
-            return interaction.editReply({ content: 'えむbot開発室は既にあります', ephemeral: true });
-        }
-
-        // なかった場合は作成
-        try {
-            const createdChannel = await guild.channels.create({
-                name: channelName,
-                type: ChannelType.GuildText // テキストチャンネルです
-            });
-            // embedを送信
-            const embed = new EmbedBuilder()
-                .setColor('#f8b4cb')
-                .setTitle('えむbot開発室')
-                .setDescription('お知らせチャンネルを作成しました')
-                .setTimestamp()
-                .setFooter({ text: 'Emubot | announce-create' });
-
-            console.log(`${createdChannel.id}を作成`);
-            await interaction.editReply({ embeds: [embed] });
-        } catch (error) {
-            console.error('アナウンスの作成エラー', error);
-            let errorMessage = 'エラーが発生しました';
-
-            // チャンネル上限エラーが発生したときのエラー
-            if (error.message.includes('Maximum number of channels reached')) {
-                errorMessage = 'チャンネル上限のため作成できませんでした。';
+            await interaction.editReply({ content: 'えむbot開発室は既にあります', ephemeral: true });
+        } else {　// なかった場合は作成
+            try {
+                const createdChannel = await guild.channels.create({
+                    name: channelName,
+                    type: ChannelType.GuildText // テキストチャンネルです
+                });
+                // embedを送信
+                const embed = new EmbedBuilder()
+                  .setColor('#f8b4cb')
+                  .setTitle('えむbot開発室')
+                  .setDescription('お知らせチャンネルを作成しました')
+                  .setTimestamp()
+                  .setFooter({ text:'Emubot | announce-create', iconURL: 'https://png.pngtree.com/png-vector/20240507/ourlarge/pngtree-announcement-icon-megaphone-vector-illustration-png-image_12366388.png'});
+                
+                console.log(`${createdChannel.id}を作成`);
+                await interaction.editReply({ embeds:[embed]});
+            } catch (error) {
+                console.error('アナウンスの作成エラー', error);
+                await interaction.editReply({ content: 'エラーが発生しました。\n```えむbot開発室```を手動で作成をお願いします..', ephemeral: true });
             }
-
-            // 2要素認証エラーが発生した時
-            if (error.message.includes('Two factor authentication is required')) {
-                errorMessage = '2要素認証が必要なため作成できませんでした。手動で'+'```'+'えむbot開発室'+'```'+'という名前のチャンネルを作ってください。';
-            }
-
-            await interaction.editReply({ content: errorMessage, ephemeral: true });
         }
     },
 };
