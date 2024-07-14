f2243fd52bbc9860d431836e56124035d93f1fa2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-14 11:20:49 +0900

    Update ping.js

--- modules/commands/ping.js
+++ modules/commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('ping値の表示'),
+    .setDescription('ping'),
   async execute(interaction) {
 
     // embedの送信
@@ -12,10 +12,10 @@ module.exports = {
       .setTitle('Emubot｜ping 🏓')
       .setDescription('Ping値')
       .setTimestamp()
-      .setFooter({ text:'Emubot | Ping'})
+      .setFooter({ text:'Emubot | Ping', iconURL:'https://cdn.icon-icons.com/icons2/1633/PNG/512/52744pingpong_109378.png' })
       .addFields(
-        { name: 'WebSocket Ping', value: '``'+`${interaction.client.ws.ping}ms`+'``', inline: true },
-        { name: 'API-Endpoint Ping', value: '``'+`${Date.now() - interaction.createdTimestamp}ms`+'``', inline: true },
+        { name: 'WebSocket Ping', value: `${interaction.client.ws.ping}ms`, inline: true },
+        { name: 'API-Endpoint Ping', value: `${Date.now() - interaction.createdTimestamp}ms`, inline: true },
         );
 
     try {
