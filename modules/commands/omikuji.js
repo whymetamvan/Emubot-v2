2c1c2e5a8d1e087113a6125659e054c47fb627a9
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-14 11:19:22 +0900

    Update omikuji.js

--- modules/commands/omikuji.js
+++ modules/commands/omikuji.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const path = require('path');
 
 const fortunes = ["大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"];
+const specialFortune = "わんだほーい！";
 const dailyFortunes = new Map();
 
 module.exports = {
@@ -12,29 +13,33 @@ module.exports = {
     await interaction.deferReply();
     try {
       const userId = interaction.user.id;
-      const thumbnailPath = path.join(__dirname, '../../lib/images/omikuji.png');
 
       // おみくじ、embedの送信
       if (!dailyFortunes.has(userId) || !isToday(dailyFortunes.get(userId))) {
         let result = '';
         do {
-          result = fortunes[Math.floor(Math.random() * fortunes.length)];
+          // 特別なおみくじの出る確率を低く設定
+          const random = Math.random();
+          if (random < 0.01) { // 1%の確率で「わんだほーい！」が出る
+            result = specialFortune;
+          } else {
+            result = fortunes[Math.floor(Math.random() * fortunes.length)];
+          }
         } while (dailyFortunes.get(userId) === result); 
 
         const embed = new EmbedBuilder()
           .setTitle('おみくじ結果')
           .setDescription(`今日の<@${interaction.user.id}>は **${result}** だよ！\nまた明日引いてね！`)
           .setTimestamp()
-          .setFooter({ text:'Emubot | omikuji'})
-
-          .setThumbnail(`attachment://${path.basename(thumbnailPath)}`)
+          .setFooter({ text:'Emubot | omikuji', iconURL:'https://tsukatte.com/wp-content/uploads/2021/10/omikuji.png' })
+          .setThumbnail(`attachment://${result === specialFortune ? 'special.png' : 'omikuji.png'}`)
           .setColor('#f8b4cb');
 
         await interaction.editReply({
           embeds: [embed],
           files: [{
-            attachment: thumbnailPath,
-            name: path.basename(thumbnailPath)
+            attachment: result === specialFortune ? specialThumbnailPath : thumbnailPath,
+            name: result === specialFortune ? 'special.png' : 'omikuji.png'
           }]
         });
 
@@ -49,6 +54,9 @@ module.exports = {
     }
   },
 };
+
+const thumbnailPath = path.join(__dirname, '../../lib/images/omikuji.png');
+const specialThumbnailPath = path.join(__dirname, '../../lib/images/special.png');
 
 function isToday(date) {
   const today = new Date();
