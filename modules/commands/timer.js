a1a145100fe06357b3dd785eecd6fdae7b8f952a
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-14 11:27:34 +0900

    Update timer.js

--- modules/commands/timer.js
+++ modules/commands/timer.js
@@ -14,20 +14,18 @@ module.exports = {
         .setRequired(false)),
   async execute(interaction) {
     try {
-      // それぞれ取得
       const minutes = interaction.options.getInteger('分');
       const seconds = interaction.options.getInteger('秒') || 0;
       const totalSeconds = (minutes * 60) + seconds;
 
       await interaction.reply(`タイマーを ${minutes} 分 ${seconds} 秒で起動します。`);
 
-      // 設定時間が経過後embedを送信
       setTimeout(() => {
         const embed = new EmbedBuilder()
           .setColor('#f8b4cb')
           .setTitle('時間になりました')
           .setTimestamp()
-          .setFooter({ text:'Emubot | timer'})
+          .setFooter({ text:'Emubot | timer', iconURL:'https://cdn.icon-icons.com/icons2/2854/PNG/512/timer_time_performance_icon_181569.png' })
           .setDescription(`${minutes}分${seconds}秒が経過しました！`);
 
         interaction.channel.send({ content: `${interaction.user}`, embeds: [embed] });
