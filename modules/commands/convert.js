6f79b9a6d04e76db0b5f560e57eb2c61cafa34d3
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-18 15:42:08 +0900

    Update convert.js

--- modules/commands/convert.js
+++ modules/commands/convert.js
@@ -1,5 +1,7 @@
-const { SlashCommandBuilder,EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const path = require('path');
+const { generate: generateGenhera } = require('genhera');
+const { generate: generateCjp } = require('cjp');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,24 +12,27 @@ module.exports = {
         .setDescription('変換タイプを選択します。')
         .setRequired(true)
         .addChoices(
-          { name: "rune文字 (英語のみ)", value: "rune" }, 
-          { name: "フェニキア文字 (英語のみ)", value: "phoenicia" },
-          { name: "ヒエログリフ (英語のみ)", value: "hieroglyphs" },
+          { name: "rune文字", value: "rune" },
+          { name: "フェニキア文字", value: "phoenicia" },
+          { name: "ヒエログリフ", value: "hieroglyphs" },
           { name: "逆読み", value: "reverse" },
-          { name: "アナグラム", value: "anagram" }
+          { name: "アナグラム", value: "anagram" },
+          { name: "ﾒﾝﾍﾗ文生成", value: "genhera" },
+          { name: "怪しい日本語生成", value: "cjp" }
         )
     )
     .addStringOption(option =>
       option.setName('text')
         .setDescription('変換するテキストを入力してください。')
-        .setRequired(true)),
+        .setRequired(true)
+    ),
 
   async execute(interaction) {
     const type = interaction.options.getString('type');
     const text = interaction.options.getString('text');
     const conversionData = require(path.join(__dirname, '..', '..', 'lib', 'data', 'convert.json'));
 
-    if (hasMention(text)) {
+    if (/^<@!?(\d+)>$/.test(text)) {
       await interaction.reply({ content: 'メンションが含まれているため、変換を行いません。', ephemeral: true });
       return;
     }
@@ -49,31 +54,31 @@ module.exports = {
       case 'anagram':
         convertedText = anagram(text);
         break;
+      case 'genhera':
+        convertedText = generateGenhera(text);
+        break;
+      case 'cjp':
+        convertedText = generateCjp(text);
+        break;
       default:
         await interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
         return;
     }
-    await interaction.deferReply();
 
     const embed = new EmbedBuilder()
       .setColor('#f8b4cb')
       .setTitle('変換完了！')
-    .setDescription(`${type}に変換しました` + "\n" +'```\n' + `${convertedText}` + '\n```' ) 
-    .setTimestamp()
-    .setFooter({ text:'Emubot | convert', iconURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4No8kzYwqTUgca_83uUhfOSJzFqbLaTM_VQJY1qbQ1m2szo_ZNEmilQfYNDEbW_a1SOo&usqp=CAU'});
-    await interaction.editReply({ embeds:[embed] });
+      .setDescription('```'+convertedText+'```')
+      .setTimestamp()
+      .setFooter({ text: 'Emubot | convert', iconURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4No8kzYwqTUgca_83uUhfOSJzFqbLaTM_VQJY1qbQ1m2szo_ZNEmilQfYNDEbW_a1SOo&usqp=CAU' });
+
+    await interaction.deferReply();
+    await interaction.editReply({ embeds: [embed] });
   },
 };
 
-function hasMention(text) {
-  return /<@!?(\d+)>/.test(text);
-}
-
 function convertText(text, conversionMap) {
-  return text.toUpperCase()
-    .split('')
-    .map(char => conversionMap[char] || char)
-    .join('');
+  return text.toUpperCase().split('').map(char => conversionMap[char] || char).join('');
 }
 
 function anagram(text) {
