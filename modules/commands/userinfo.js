f7656d2a854de9a422ca5d0ec4ac54ba1f118862
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-07-23 10:13:00 +0900

    Update userinfo.js

--- modules/commands/userinfo.js
+++ modules/commands/userinfo.js
@@ -4,64 +4,33 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('userinfo')
         .setDescription('ユーザーの検索')
-        .addUserOption(option => 
-            option.setName('user')
-                .setDescription('ユーザーIDかメンションを入力してください')
-                .setRequired(true)),
+        .addUserOption(option => option.setName('user').setDescription('ユーザーIDかメンションを入力してください').setRequired(true)),
+
     async execute(interaction) {
         try {
+            await interaction.deferReply();
             const user = interaction.options.getUser('user');
-
-            if (!user) {
-                return interaction.reply('ユーザーが見つかりませんでした。');
-            }
-
-            let member;
-            try {
-                member = await interaction.guild.members.fetch(user.id);
-            } catch (error) {
-                console.log('ユーザーはサーバーのメンバーではありません');
-            }
-
-            const avatarURL = member ? member.displayAvatarURL({ size: 1024 }) : user.displayAvatarURL({ size: 1024 });
+            const member = user ? await interaction.guild.members.fetch(user.id).catch(() => null) : null;
+            const avatarURL = member?.displayAvatarURL({ size: 1024 }) || user.displayAvatarURL({ size: 1024 });
 
             const embed = new EmbedBuilder()
                 .setColor('#f8b4cb')
-                .setTimestamp()
-                .setFooter({ text: 'Emubot | userinfo', iconURL: interaction.client.user.displayAvatarURL() })
                 .setTitle('ユーザー情報')
                 .setThumbnail(avatarURL)
+                .setFooter({ text: 'Emubot | userinfo', iconURL: interaction.client.user.displayAvatarURL() })
                 .addFields(
                     { name: 'ユーザー名', value: user.tag },
-                    { name: 'ユーザーID', value: '```\n'+`${user.id}`+ '\n```' },
-                    { name: 'アカウント作成日', value: user.createdAt ? `${user.createdAt.toLocaleString('ja-JP')}` : '不明', inline: true },
-                    { name: 'プロフィール', value: `<@${user.id}>`, inline:true }
+                    { name: 'ユーザーID', value: `\`\`\`\n${user.id}\n\`\`\`` },
+                    { name: 'アカウント作成日', value: user.createdAt ? user.createdAt.toLocaleString('ja-JP') : '不明', inline: true },
+                    { name: 'プロフィール', value: `<@${user.id}>`, inline: true },
+                    { name: 'サーバー参加日', value: member ? member.joinedAt ? member.joinedAt.toLocaleString('ja-JP') : '不明' : '未参加' },
+                    { name: 'AccountType', value: user.bot ? 'BOT 🤖' : 'USER <:user:1254362184272707676>', inline: true }
                 );
 
-            if (member) {
-                embed.addFields(
-                    { name: 'サーバー参加日', value: member.joinedAt ? `${member.joinedAt.toLocaleString('ja-JP')}` : '不明' }
-                );
-            } else { 
-                embed.addFields(
-                    { name: 'サーバー参加日', value: '未参加' }
-                );
-            }
-
-            if (user.bot) { 
-                embed.addFields(
-                    { name: 'AccountType', value: 'BOT 🤖' }
-                );
-            } else { 
-                embed.addFields(
-                    { name: 'AccountType', value: 'USER <:user:1254362184272707676>', inline: true }
-                );
-            }
-
-            await interaction.reply({ embeds: [embed] });
+            await interaction.editReply({ embeds: [embed] });
         } catch (error) {
             console.error(error);
-            await interaction.reply('エラーが発生しました。コマンドを実行できませんでした。');
+            await interaction.editReply('エラーが発生しました。コマンドを実行できませんでした。');
         }
     },
 };
