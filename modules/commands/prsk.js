385f1bdbb32f24ca31903af646e93fd341990573
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-08-15 21:49:10 +0900

    Update prsk.js

--- modules/commands/prsk.js
+++ modules/commands/prsk.js
@@ -31,6 +31,11 @@ module.exports = {
       return;
     }
 
+    if (count > 20) {
+      await interaction.reply('曲数は20以下で指定してください。');
+      return;
+    }
+    
       await interaction.deferReply();
     try {
       const songList = fs.readFileSync(dataFilePath, 'utf8').split('\n').filter(song => song.trim() !== '');
