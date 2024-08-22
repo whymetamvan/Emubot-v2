e54553a676762fd313b2468028143aa7459c1230
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-08-22 14:55:28 +0900

    Update chunithm.js

--- modules/commands/chunithm.js
+++ modules/commands/chunithm.js
@@ -13,14 +13,12 @@ module.exports = {
         .addChoices(
           { name: '全曲', value: 'all' },
           { name: 'オリジナルのみ', value: 'original' },
-          { name: 'WE譜面&ULTIMAのみ', value: 'weul' }
-        )
-    )
+          { name: 'WE譜面&ULTIMAのみ', value: 'weul' },
+        ))
     .addIntegerOption(option =>
       option.setName('count')
         .setDescription('選択する曲の数を指定します。')
-        .setRequired(true)
-    ),
+        .setRequired(true)),
 
   async execute(interaction) {
     await interaction.deferReply();
@@ -28,17 +26,14 @@ module.exports = {
     const count = interaction.options.getInteger("count");
     const dataFilePath = path.join(__dirname, '..', '..', 'lib', 'random', 'chunithm', `${option.toUpperCase()}.txt`);
 
-
     if (!['all', 'original', 'weul'].includes(option)) {
       await interaction.editReply('選択肢から選んでください：```all、level10、weil```');
       return;
     }
-
-    if (count > 20) {
-      await interaction.editReply('曲数は20以下で指定してください。');
+    if (count < 1 || count > 20) {
+      await interaction.editReply('曲数は1から20の間で指定してください。');
       return;
     }
-
     try {
       const songList = fs.readFileSync(dataFilePath, 'utf8').split('\n').filter(song => song.trim() !== '');
 
@@ -46,11 +41,6 @@ module.exports = {
         await interaction.editReply('曲が見つかりませんでした。');
         return;
       }
-      if (count < 1 || count > songList.length) {
-        await interaction.editReply('曲数は1以上、曲リストの総数以下で指定してください。');
-        return;
-      }
-
       const selectedSongs = [];
       while (selectedSongs.length < count) {
         const randomSong = songList[Math.floor(Math.random() * songList.length)];
@@ -65,7 +55,6 @@ module.exports = {
         .setTimestamp()
         .setFooter({ text: 'Emubot | chunithm', iconURL: 'https://pbs.twimg.com/profile_images/1735059307195256832/6YSNiEle_400x400.jpg' })
         .setColor('#fffc3c');
-
       await interaction.editReply({ embeds: [embed] });
     } catch (error) {
       console.error(error);
